8821e41af8e217a9047cdf168aa5688fae9ebcb7
Marius <marius@example.com>
2022-10-10 11:07:10 +0300

    uzd1 variables

---
+++ uzd1.js
@@ -0,0 +1,10 @@
+'use strict';
+// 1. susikurti kintamuosius vardas, salis, megstamiausiasSkaicius, megstamiausia savaites diena (skaiciais 1-7)
+
+// 2. isvedam i konsole visus savo kintamiuosiu atskirai
+
+// 3. atspausdinam konsoleje savo megstamiausio skaiciau ir dienos daugyba
+
+// 4. atspausdinam konsoleje savo megstamiausio skaiciau is dienos sudeti
+
+// 5. atspausdinam konsoleje naudodami kintamuosius "as esu John. noreciau keliausi i Jamaika. Mano megstamiausias skaicius yra 777"
